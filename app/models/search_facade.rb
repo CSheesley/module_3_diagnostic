6d03af8b5d4ef30aed5a392ee27c62414a3d46a0
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-13 10:37:34 -0600

    Builds out Station PORO with attributes - need to check if working

--- app/models/search_facade.rb
+++ app/models/search_facade.rb
@@ -6,9 +6,9 @@ class SearchFacade
   end
 
   def all_stations
-    response = conn.get
-    data = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
-    binding.pry
+    station_data.map do |station_data|
+      Station.new(station_data)
+    end
   end
 
   # def closest_stations
@@ -18,16 +18,22 @@ end
 
 private
 
+def station_data
+  response = conn.get
+  data = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+end
+
 def conn
   Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest") do |f|
   f.params["api_key"] = ENV["NREL_API_KEY"]
   f.params["location"] = @query
   f.params["fuel_type"] = "ELEC,LPG"
   f.params["access"] = "public"
+  f.params["limit"] = 100
   f.params["format"] = "json"
   f.adapter Faraday.default_adapter
   end
 
 end
 # successful API Call
-# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?api_key=gdorVUbVrOWclFOuNkt2SmWPXkYbyFd9nCefRdzV&location=80206&fuel_type=ELEC,LPG&access=public&format=json
+# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?api_key=gdorVUbVrOWclFOuNkt2SmWPXkYbyFd9nCefRdzV&location=80206&fuel_type=ELEC,LPG&access=public&limit=100&format=json
