5b0d54fc81b384e26c2078ab6394664ae0b10818
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-13 09:53:57 -0600

    Made successfull API call? working on setting up Faraday

--- app/models/search_facade.rb
+++ app/models/search_facade.rb
@@ -5,4 +5,25 @@ class SearchFacade
     @query = query
   end
 
+  def all_stations
+  # API CALL
+  end
+
+  # def closest_stations
+  # end
+
 end
+
+private
+
+def conn
+  Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest") do |f|
+
+
+  end
+
+
+
+end
+# successful API Call
+# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?api_key=gdorVUbVrOWclFOuNkt2SmWPXkYbyFd9nCefRdzV&location=80206&format=json
