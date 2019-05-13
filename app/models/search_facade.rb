c4bf0df8258c319eda664a188b6279da9b76e63c
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-13 10:02:40 -0600

    Working on formatting API repsonse - about to create Station models

--- app/models/search_facade.rb
+++ app/models/search_facade.rb
@@ -6,7 +6,9 @@ class SearchFacade
   end
 
   def all_stations
-  # API CALL
+    response = conn.get
+    data = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+    binding.pry
   end
 
   # def closest_stations
@@ -18,11 +20,11 @@ private
 
 def conn
   Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest") do |f|
-
-
+  f.params["api_key"] = ENV["NREL_API_KEY"]
+  f.params["location"] = @query
+  f.params["format"] = "json"
+  f.adapter Faraday.default_adapter
   end
-
-
 
 end
 # successful API Call
