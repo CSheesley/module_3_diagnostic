dfe71133cb54e9b9a84b43eafe8030e5c43ca9db
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-13 09:21:04 -0600

    Completes initial user story spec set up

--- spec/features/user_can_get_search_results_spec.rb
+++ spec/features/user_can_get_search_results_spec.rb
@@ -8,17 +8,31 @@ describe 'As a User on the homepage' do
       visit '/'
 
       # And I fill in the search form with 80206 (Note: Use the existing search form)
-
-
-
+      fill_in :q, with: "80206"
+  
       # And I click "Locate"
+      click_on("Locate")
+
       # Then I should be on page "/search"
+      expect(current_path).to eq("/search")
+
       # Then I should see the total results of the stations that match my query, 90.
+      expect(page).to have_css('.station', count: 90)
+
       # Then I should see a list of the 15 closest stations within 5 miles sorted by distance
+      expect(page).to have_css('.closest', count: 15)
+      # sort by distance
       # And the stations should be limited to Electric and Propane
       # And the stations should only be public, and not private, planned or temporarily unavailable.
-      # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
 
+      # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+      within first('.station') do
+        expect(page).to have_content("Name: ")
+        expect(page).to have_content("Address: ")
+        expect(page).to have_content("Fuel Types: ")
+        expect(page).to have_content("Distance: ")
+        expect(page).to have_content("Access Times: ")
+      end
 
 
 
