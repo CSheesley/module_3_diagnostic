a0f187dcd7ffbfe3d214c2cecd07e42cc0b47d90
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-13 09:38:44 -0600

    Adds local variable to search#index, creates Facade PORO

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,9 @@ class SearchController < ApplicationController
   def index
     # binding.pry
     # params "q"=>"80206"
+    render locals: {
+      facade: SearchFacade.new(params[:q])
+    }
   end
 
 end
---
+++ app/models/search_facade.rb
@@ -0,0 +1,8 @@
+class SearchFacade
+
+  def initialize(query)
+    # binding.pry
+    @query = query
+  end
+
+end
